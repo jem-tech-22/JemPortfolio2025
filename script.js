1b0f9d98c8707b87355bba79f792b09c19212a2d
Charles James Salac <salaccharlesjames@gmail.com>
2025-10-24 23:43:19 +0800

    main

--- script.js
+++ script.js
@@ -30,3 +30,26 @@ if (hamburger && navMenu) {
         setTimeout(() => { scrollThrottle = false; }, 150);
     }, { passive: true });
 }
+
+    // Get elements
+    const modal = document.getElementById("imageModal");
+    const thumbnail = document.getElementById("thumbnail");
+    const fullImage = document.getElementById("fullImage");
+    const closeBtn = document.getElementsByClassName("close")[0];
+
+    thumbnail.onclick = function() {
+    modal.style.display = "block";
+    fullImage.src = this.src; // show same image but larger
+    }
+
+    // When user clicks the close button
+    closeBtn.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // Optional: close when user clicks anywhere outside the image
+    modal.onclick = function(e) {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+    }
